2d474d6c6fe0fd3e8a8c3bb5ec6dafcebb2b2027
watanabe.everystudio <watanabe.everystudio@gmail.com>
2017-01-09 22:45:41 +0900

    メッセージウインドとか使う予定のものを追加 アイテムは組み合わせデータとかを用意しないとだな

---
+++ Assets/00game/script/ItemButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Sprite))]
+public class ItemButton : MonoBehaviour {
+
+	public int m_iItemId;
+	public int m_iIndex;
+	public Sprite m_sprImage;
+
+	public void Initialize( int _iIndex , ItemManager _manager)
+	{
+		m_iIndex = _iIndex;
+		m_sprImage = gameObject.GetComponent<Image>().sprite;
+
+		gameObject.GetComponent<Button>().onClick.AddListener(() =>
+		{
+			_manager.OnPushed(this);
+		});
+	}
+
+	public void Set( int _iItemId , Sprite _sprite)
+	{
+		m_iItemId = _iItemId;
+		m_sprImage = _sprite;
+	}
+}
---
+++ Assets/00game/script/ItemManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : Singleton<ItemManager> {
+
+	[SerializeField]
+	private List<ItemButton> item_button_list = new List<ItemButton>();
+
+	public ItemButton selecting;
+
+	public override void Initialize()
+	{
+		selecting = null;
+		base.Initialize();
+		int iIndex = 0;
+		foreach( ItemButton button in item_button_list)
+		{
+			button.Initialize(iIndex,this);
+			iIndex += 1;
+		}
+	}
+
+	public void OnPushed(ItemButton _button)
+	{
+
+	}
+
+
+
+
+}
